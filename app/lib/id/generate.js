2331722f27302c284f5f51742ce4bbc3373947bf
neon-x-hub <abdomem7@gmail.com>
2025-08-03 19:25:27 +0100

    Feature: support custom length in ID generation

--- app/lib/id/generate.js
+++ app/lib/id/generate.js
@@ -1,4 +1,12 @@
-function generateId(length) {
+/**
+ * Generates a random string of characters of a given length, using the
+ * given alphabet of characters. The generated string is suitable for
+ * use as a unique identifier.
+ *
+ * @param {number} length - The length of the string to generate.
+ * @returns {string} A random string of the given length.
+ */
+function generateId(length = 10) {
     const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
     let id = '';
     for (let i = 0; i < length; i++) {
