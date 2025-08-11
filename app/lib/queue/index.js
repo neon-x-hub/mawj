03e8ec2ae88ff6407e75ab3cce3998f413889022
neon-x-hub <abdomem7@gmail.com>
2025-08-11 01:45:08 +0100

    Feature: make queue enqueueing depend on project type (card or video)

--- app/lib/queue/index.js
+++ app/lib/queue/index.js
@@ -1,14 +1,17 @@
 import InMemoryQueue from './InMemory.js';
 import { workerRenderer } from '@/app/lib/engine/image/worker.js';
-
+import { workerVideoRenderer } from '../engine/video/worker.js';
 const globalForQueue = globalThis;
 
 if (!globalForQueue.__QUEUE__) {
-  globalForQueue.__QUEUE__ = new InMemoryQueue(2);
+    globalForQueue.__QUEUE__ = new InMemoryQueue(2);
 }
 
 export const queue = globalForQueue.__QUEUE__;
 
 export function enqueueRenderJob(jobData) {
-  return queue.addJob(jobData, workerRenderer);
+    if (jobData.project.type === 'video') {
+        return queue.addJob(jobData, workerVideoRenderer);
+    }
+    return queue.addJob(jobData, workerRenderer);
 }
