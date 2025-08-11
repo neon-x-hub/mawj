e8a5f86a2657a03d1f4e9eff33ef94d481907e3c
neon-x-hub <abdomem7@gmail.com>
2025-08-12 00:56:08 +0100

    Feature: add template text section for images in ImageEditPanel

--- app/components/core/menu/ImageEditPanel.js
+++ app/components/core/menu/ImageEditPanel.js
@@ -9,6 +9,7 @@ import {
 import { t, getLang } from "@/app/i18n";
 // Sections
 import TransformSection from "@/app/components/features/templates/edit_page/panel/transformations/TransformSection";
+import TextTemplateSection from "../../features/templates/edit_page/panel/text_template/TextTemplateSection";
 
 export const ImagePropertiesPanel = ({ value, onChange }) => {
 
@@ -17,6 +18,8 @@ export const ImagePropertiesPanel = ({ value, onChange }) => {
     return (
         <Card className="w-full" >
             <CardBody className={`flex flex-col gap-6 ${getLang() === "ar" ? "text-right" : ""} `}>
+                <TextTemplateSection value={value} update={update} />
+                <Divider />
                 {/* Section: Transformations */}
                 <TransformSection value={value} update={update} />
                 <Divider />
