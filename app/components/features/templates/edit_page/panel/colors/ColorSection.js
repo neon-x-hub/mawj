5e90c3afabe4112eb4015118aff3bfb9f591f7e0
neon-x-hub <abdomem7@gmail.com>
2025-09-20 23:05:04 +0100

    Feature: add opacity option to ColorSection

--- app/components/features/templates/edit_page/panel/colors/ColorSection.js
+++ app/components/features/templates/edit_page/panel/colors/ColorSection.js
@@ -3,6 +3,7 @@ import { t } from '@/app/i18n'
 
 import ColorPickerSection from './ColorPickerSec'
 import PanelSectionTitle from '../SectionTitle'
+import { NumberInput } from '@heroui/react'
 
 export default function ColorSection({ value, update }) {
     return (
@@ -17,6 +18,15 @@ export default function ColorSection({ value, update }) {
                 }}
             />
 
+            <NumberInput
+                label={t("layers.text.opacity")}
+                value={value.opacity}
+                step={0.01}
+                max={1}
+                min={0}
+                onValueChange={(newOpacity) => update({ opacity: newOpacity })}
+            />
+
             <ColorPickerSection
                 color={value.color}
                 onChange={(newColor) => update({ color: newColor })}
@@ -28,6 +38,8 @@ export default function ColorSection({ value, update }) {
                 onChange={(newColor) => update({ backgroundColor: newColor })}
                 label={t("layers.text.backgroundColor")}
             />
+
+
 
         </section>
     )
