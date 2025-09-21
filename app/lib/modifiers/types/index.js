6394a81626b6ef834ecd92cfc788fb7a18fe8aab
neon-x-hub <abdomem7@gmail.com>
2025-09-21 23:24:57 +0100

    Feature: implement modifier builder function

--- app/lib/modifiers/types/index.js
+++ app/lib/modifiers/types/index.js
@@ -1,4 +1,15 @@
 import FadeModifier from "./FadeModifier";
 
+const modifiers = {
+    fade: FadeModifier
+};
 
-export { FadeModifier };
+function buildModifier(id, data) {
+    if (!data || !data.type || !modifiers[data.type]) {
+        throw new Error("Invalid modifier type");
+    }
+    const ModifierClass = modifiers[data.type];
+    return new ModifierClass({ id, ...data });
+}
+
+export { FadeModifier, buildModifier };
